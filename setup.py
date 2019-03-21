1faf9a3ab597b7f2f16112779de218577a14ce3b
KitaharaMugiro <ngthornym@yahoo.co.jp>
2019-03-21 12:21:46 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'Mate',
+    version = '1.0.0',
+    url = 'https://github.com/KitaharaMugiro/material_scripts.git',
+    license = 'Free',
+    author = 'Mugiro Kitahara',
+    author_email = 'ngthornym@me.com',
+    description = 'scripts for better development',
+    install_requires = ['setuptools' , 'requests'],
+    packages = ["scripts"]
+)
